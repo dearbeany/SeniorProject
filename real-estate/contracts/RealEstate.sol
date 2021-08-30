a8819d8f2b9c6304337b3e675290587c04e7cb6b
dearbeany <980429sb@naver.com>
2021-08-30 23:37:04 +0900

    'age' to 'email' 변경 commit

--- real-estate/contracts/RealEstate.sol
+++ real-estate/contracts/RealEstate.sol
@@ -5,7 +5,7 @@ contract RealEstate {
     struct Buyer {
         address buyerAddress;
         string name;
-        uint256 age;
+        string email;
     }
 
     mapping(uint256 => Buyer) public buyerInfo; //매입자 정보를 불러오는 함수
@@ -22,12 +22,12 @@ contract RealEstate {
     function buyRealEstate(
         uint256 _id,
         string memory _name,
-        uint256 _age
+        string memory _email
     ) public payable {
         //매개변수=매입자 정보
         require(_id >= 0 && _id <= 9); //유효성 체크. 매개변수로 받은 매물의 아이디 0~9 사이
         buyers[_id] = msg.sender; //msg.sender가 현재 이 함수를 사용하고 있는 계정
-        buyerInfo[_id] = Buyer(msg.sender, _name, _age); //매입자 정보를 struct에 저장
+        buyerInfo[_id] = Buyer(msg.sender, _name, _email); //매입자 정보를 struct에 저장
 
         address(uint160(owner)).transfer(msg.value);
         emit LogBuyRealEstate(msg.sender, _id);
@@ -39,11 +39,11 @@ contract RealEstate {
         returns (
             address,
             string memory,
-            uint256
+            string memory
         )
     {
         Buyer memory buyer = buyerInfo[_id];
-        return (buyer.buyerAddress, buyer.name, buyer.age);
+        return (buyer.buyerAddress, buyer.name, buyer.email);
     } // read only
 
     function getAllBuyers() public view returns (address[10] memory) {
